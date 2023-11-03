b1c74b6857557effab8af0d34b1d3420a2bb8114
dashma518 <mayeboy518@gmail.com>
2023-11-03 15:38:06 -0400

    Created pages for TermToolXref for manual insertion, update, and deletion. Now records do not need to be loaded upon initialization.

---
+++ TCT/Pages/TermToolXrefs/ToolSelectPageModel.cs
@@ -0,0 +1,26 @@
+﻿using TCT.Data;
+using TCT.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace TCT.Pages.TermToolXrefs
+{
+    public class ToolSelectPageModel : PageModel
+    {
+        public SelectList ToolNameSL { get; set; }
+
+        public void PopulateToolsDropDownList(TCTContext _context,
+            object selectedTool = null)
+        {
+            var toolsQuery = from t in _context.Tools
+                             orderby t.InternalId // Sort by InternalId
+                             select t;
+
+            ToolNameSL = new SelectList(toolsQuery.AsNoTracking(),
+                    nameof(Tool.Id),
+                    nameof(Tool.InternalId),
+                    selectedTool);
+        }
+    }
+}
